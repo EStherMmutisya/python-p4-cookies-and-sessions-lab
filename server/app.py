f531dba9027a08de23640af23ed893040b742649
EStherMmutisya <starjacob994@gmail.com>
2024-04-14 17:51:33 +0300

    cookies lab

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
 
@@ -17,18 +15,32 @@ db.init_app(app)
 
 @app.route('/clear')
 def clear_session():
-    session['page_views'] = 0
+    session.pop('page_views', None)
     return {'message': '200: Successfully cleared session data.'}, 200
-
-@app.route('/articles')
-def index_articles():
-
-    pass
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    # Initialize page_views to 0 if it doesn't exist in session
+    page_views = session.get('page_views', 0)
+    
+    # Increment page_views for each request
+    page_views += 1
+    session['page_views'] = page_views
+    
+    # Check if the user has viewed more than 3 pages
+    if page_views > 3:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
 
-    pass
+    # Retrieve the article from the database
+    article = Article.query.get_or_404(id)
+    
+    # Return article data in JSON response
+    return jsonify({
+        'id': article.id,
+        'title': article.title,
+        'content': article.content,
+        # Add other article attributes as needed
+    })
 
 if __name__ == '__main__':
     app.run(port=5555)
